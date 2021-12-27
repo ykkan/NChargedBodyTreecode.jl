acefc3ad6741a2cbc534a6c7d1638a6911581786
= <=>
2021-12-27 10:53:05 +0100

    refactor realtivity.jl

--- src/relativity.jl
+++ src/relativity.jl
@@ -1,69 +1,39 @@
-function particle2frame!(beam::Particles, p_avg::SVector{3,T}) where {T}
-    g_avg = sqrt(1.0 + dot(p_avg, p_avg))
+function transformPosition(x::SVector{3,T}, fg::T, fp::SVector{3,T}) where {T}
+    return x + 1.0/(fg + 1.0) * dot(fp, x)*fp
+end
+
+function transformMomentum(p::SVector{3,T}, fg::T, fp::SVector{3,T}) where {T}
+    g = sqrt(1.0 + dot(p,p))
+    return  p + 1.0/(fg + 1.0) * dot(fp, p)*fp - g*fp
+end
+
+function transformParticleDistribution!(beam::Particles, fp::SVector{3,T}) where {T}
+    fg = sqrt(1.0 + dot(fp, fp))
     pos = beam.positions
     mom = beam.momentums
     for k in 1:beam.npar
         x = pos[k]
         p = mom[k]
         g = sqrt(1.0 + dot(p, p))
-        pos[k] = x + 1.0/(g_avg + 1.0) * dot(p_avg, x)*p_avg
-        mom[k] = p + 1.0/(g_avg + 1.0) * dot(p_avg, p)*p_avg - g*p_avg
+        pos[k] = transformPosition(x, fg, fp)
+        mom[k] = transformMomentum(p, fg, fp)
     end
 end
 
-function lab2avgframe!(beam::Particles, p_avg::SVector{3,T}) where {T}
-    g_avg = sqrt(1.0 + dot(p_avg, p_avg))
-    pos = beam.positions
-    mom = beam.momentums
-    for k in 1:beam.npar
-        x = pos[k]
-        p = mom[k]
-        g = sqrt(1.0 + dot(p, p))
-        pos[k] = x + 1.0/(g_avg + 1.0) * dot(p_avg, x)*p_avg
-        mom[k] = p + 1.0/(g_avg + 1.0) * dot(p_avg, p)*p_avg - g*p_avg
-    end
-end
-
-function avg2labframe!(beam::Particles, p_avg::SVector{3, T}) where {T}
-    g_avg = sqrt(1.0 + dot(p_avg, p_avg))
-    p_avg_u = p_avg / norm(p_avg)
-    pos = beam.positions
-    mom = beam.momentums
-    for k in 1:beam.npar
-        x = pos[k]
-        p = mom[k]
-        g = sqrt(1.0 + dot(p, p))
-        pos[k] = x + (1.0/g_avg - 1.0)*dot(x, p_avg_u)*p_avg_u
-        mom[k] = p + 1.0/(g_avg + 1.0)*dot(p_avg, p)*p_avg + g*p_avg
-    end
-end
-
-function field2frame(efield::SVector{3, T}, bfield::SVector{3, T}, g_f::T, p_f::SVector{3, T}) where {T}
-  efield_f = g_f*efield + cross(p_f, bfield) - 1.0/(g_f + 1.0)*dot(p_f, efield)*p_f
-  bfield_f = g_f*bfield - cross(p_f, efield) - 1.0/(g_f + 1.0)*dot(p_f, bfield)*p_f
+function transformEMField(efield::SVector{3, T}, bfield::SVector{3, T}, fg::T, fp::SVector{3, T}) where {T}
+  efield_f = fg*efield + cross(fp, bfield) - 1.0/(fg + 1.0)*dot(fp, efield)*fp
+  bfield_f = fg*bfield - cross(fp, efield) - 1.0/(fg + 1.0)*dot(fp, bfield)*fp
   return efield_f, bfield_f
 end
 
 
-function fields2frame!(beam::Particles, p_f::SVector{3, T}) where {T}
-  g_f = sqrt(1.0 + dot(p_f, p_f))
+function transformParticlesField!(beam::Particles, fp::SVector{3, T}) where {T}
+  fg = sqrt(1.0 + dot(fp, fp))
   efields = beam.self_efields
   bfields = beam.self_bfields
   for i in 1:beam.npar
-    efield, bfield = field2frame(efields[i], bfields[i], g_f, p_f)
-    efields[i] = efield
-    bfields[i] = bfield
+    efield_f, bfield_f = transformEMField(efields[i], bfields[i], fg, fp)
+    efields[i] = efield_f
+    bfields[i] = bfield_f
   end
-end
-
-function field2labframe(e_p::SVector{3, T} , b_p::SVector{3, T}, g_avg::T, p_avg::SVector{3, T}) where {T}
-    efield = g_avg*e_p - cross(p_avg, b_p) - 1.0/(g_avg + 1.0)*dot(p_avg, e_p)*p_avg
-    bfield = g_avg*b_p + cross(p_avg, e_p) - 1.0/(g_avg + 1.0)*dot(p_avg, b_p)*p_avg
-    return efield, bfield
-end
-
-function field2avgframe(e_p::SVector{3, T} , b_p::SVector{3, T}, g_avg::T, p_avg::SVector{3, T}) where {T}
-    efield = g_avg*e_p + cross(p_avg, b_p) - 1.0/(g_avg + 1.0)*dot(p_avg, e_p)*p_avg
-    bfield = g_avg*b_p - cross(p_avg, e_p) - 1.0/(g_avg + 1.0)*dot(p_avg, b_p)*p_avg
-    return efield, bfield
 end
--- src/update_particles_field.jl
+++ src/update_particles_field.jl
@@ -3,11 +3,12 @@ function update_particle_field!(particles::Particles; n, ita, threshold, lambda)
     p_avg = sum(particles.momentums) / particles.npar
 
     ct = ClusterTree(particles; n=n, threshold=threshold)
+    amp = 2.8179403699772166e-15 * q / lambda
     @inbounds for i in 1:particles.npar
         x = particles.positions[i]
         (efield, bfield) = cluster2p(x, ct.root, particles, ct.parindices, p_avg; n=n, ita=ita)
-        particles.self_efields[i] = (2.8179403699772166e-15 * q / lambda) * efield
-        particles.self_bfields[i] = (2.8179403699772166e-15 * q / lambda) * bfield
+        particles.self_efields[i] = amp * efield
+        particles.self_bfields[i] = amp * bfield
     end
 end
 
@@ -17,11 +18,12 @@ function update_particle_field_stretch!(particles::Particles; n, ita, threshold,
     g_avg = sqrt(1.0 + dot(p_avg, p_avg))
     stretch = SVector(1.0,1.0,g_avg)
     ct = ClusterTree(particles; n=n, threshold=threshold, stretch=stretch)
+    amp = 2.8179403699772166e-15 * q / lambda
     @inbounds for i in 1:particles.npar
         x = particles.positions[i]
         (efield, bfield) = cluster2p(x, ct.root, particles, ct.parindices, p_avg; n=n, ita=ita, stretch=stretch)
-        particles.self_efields[i] = (2.8179403699772166e-15 * q / lambda) * efield
-        particles.self_bfields[i] = (2.8179403699772166e-15 * q / lambda) * bfield
+        particles.self_efields[i] = amp * efield
+        particles.self_bfields[i] = amp * bfield
     end
 end
 
@@ -30,18 +32,21 @@ function update_particle_field_AVGRF!(particles::Particles; n, ita, threshold, l
     p_avg = sum(particles.momentums) / particles.npar
     g_avg = sqrt(1.0 + dot(p_avg, p_avg))
 
-    lab2avgframe!(particles, p_avg)
+    # transform particles to rest-frame
+    transformParticleDistribution!(particles, p_avg)
 
+    # evaluate particles field in the rest-frame
     ct = ClusterTree(particles; n=n, threshold=threshold)
+    amp = 2.8179403699772166e-15 * q / lambda
     @inbounds for i in 1:particles.npar
         x = particles.positions[i]
         (efield, bfield) = cluster2p(x, ct.root, particles, ct.parindices, SVector(0.0,0.0,0.0); n=n, ita=ita)
-        (efield, bfield) = field2labframe(efield, bfield, g_avg, p_avg)
-        particles.self_efields[i] = (2.8179403699772166e-15 * q / lambda) * efield
-        particles.self_bfields[i] = (2.8179403699772166e-15 * q/ lambda) * bfield
+        particles.self_efields[i] = amp * efield
+        particles.self_bfields[i] = amp * bfield
     end
 
-    avg2labframe!(particles, p_avg)
+    # transform particle field back to the lab-frame
+    transformParticlesField!(particles, -p_avg)
 end
 
 function update_particle_field_brutal!(particles::Particles; lambda)
@@ -51,12 +56,13 @@ function update_particle_field_brutal!(particles::Particles; lambda)
         particles.self_efields[i] = SVector(0.0, 0.0, 0.0)
         particles.self_bfields[i] = SVector(0.0, 0.0, 0.0)
         xi = particles.positions[i]
+        amp = 2.8179403699772166e-15 * q / lambda
         for j in 1:npar
             xj = particles.positions[j]
             pj = particles.momentums[j]
             K = kernel_relativity(xi, xj, pj)
-            particles.self_efields[i] += 2.8179403699772166e-15 * q / lambda * sqrt(1.0 + dot(pj, pj)) * K
-            particles.self_bfields[i] += 2.8179403699772166e-15 * q / lambda * cross(pj, K)
+            particles.self_efields[i] += amp * sqrt(1.0 + dot(pj, pj)) * K
+            particles.self_bfields[i] += amp * cross(pj, K)
         end
     end
 end
